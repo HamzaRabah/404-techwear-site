dcaa27ba0bb0b48376c57f47aa7fe711473c44c0
Hamza Rabah <h.rabah@outlook.com>
2025-05-12 11:29:13 +0300

    Updates product prices
    Reduces the price of several products.
    
    This makes the items more attractive to potential buyers.

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
         id: "no-problem-protocol",
         name: "No Problem Protocol",
         description: "Celebrating those unexpected offline moments, this design takes a common internet error and turns it into a calm, cool statement, where no internet means no problem!",
-        price: 19.99,
+        price: 16,
         images: [
             {
                 src: "assets/images/no-internet-zoom-view.webp",
@@ -24,7 +24,7 @@ const products = [
         id: "new-default",
         name: "New Default",
         description: "When search engines became fallback tools, and ChatGPT became your first stop. A subtle nod to how we code, learn, and think today.",
-        price: 19.99,
+        price: 16,
         images: [
             {
                 src: "assets/images/chatgpt-zoom-view.webp",
@@ -44,7 +44,7 @@ const products = [
         id: "works-locally",
         name: "Works Locally",
         description: "If it works here, it’s not my fault it breaks there. A badge of every dev’s quiet truth: one environment’s success is another’s headache.",
-        price: 19.99,
+        price: 16,
         images: [
             {
                 src: "assets/images/works-locally-zoom-view.webp",
